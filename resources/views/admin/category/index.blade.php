3bf94f50162f29472723d0175f128a5ec0d8e1d1
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2023-02-06 23:26:22 +0530

    fetch category data

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -20,7 +20,39 @@
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900">
                 <a href="{{ route('category.create') }}" class="inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-600 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Add New Category</a><br><br>                    
-                
+                <div class="overflow-x-auto">
+                    <table class="w-full text-base text-left text-gray-700 dark:text-gray-400">
+                        <thead class="text-sm text-gray-800 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr class="">
+                                <th class="py-3 px-6">
+                                    ID
+                                </th>
+                                <th class="py-3 px-6">
+                                    Category Name
+                                </th>
+                                <th class="py-3 px-6">
+                                    Actions
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($categories as $category)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">                                    
+                                <td class="py-4 px-6">
+                                    {{ $category->id }}
+                                </td>
+                                <td class="py-4 px-6">
+                                    {{ $category->name }}
+                                </td>
+                                <td class="py-4 px-6">
+                                    <a href="" class="inline-flex items-center px-4 py-2 bg-yellow-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-500 focus:bg-yellow-700 active:bg-yellow-900 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 transition ease-in-out duration-150" >Edit</a>                                    
+                                    <button type="button" value="{{ $category->id }}" data-modal-toggle="deleteData" class="deleteBtn inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">Delete</button>                                        
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
