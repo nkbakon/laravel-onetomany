bb691ea5839809e0fcc93c9a8ffdad7ccde902ba
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2023-02-13 14:59:32 +0530

    display data with belongsTo feature

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,4 +18,9 @@ class Product extends Model
         'slug',
         'price',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -42,7 +42,13 @@
                                     {{ $product->id }}
                                 </td>
                                 <td class="py-4 px-6">
+                                    {{ $product->category->name }}
+                                </td>
+                                <td class="py-4 px-6">
                                     {{ $product->name }}
+                                </td>
+                                <td class="py-4 px-6">
+                                    {{ $product->price }}
                                 </td>
                                 <td class="py-4 px-6">
                                     <a href="" class="inline-flex items-center px-4 py-2 bg-yellow-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-500 focus:bg-yellow-700 active:bg-yellow-900 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 transition ease-in-out duration-150" >Edit</a>                                    
